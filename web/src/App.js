79954e0ee0368dd2c679bd9424863cf4a2bf8566
Luma Marigo <luma.marigo@sp.senai.br>
2021-07-20 20:47:58 -0300

    Adicionado o projeto web

---
+++ web/src/App.js
@@ -0,0 +1,21 @@
+
+import { Row, Col } from "antd";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Alertas dos Servidores</h1>
+    <Row>
+      <Col span="3">
+        Esquerda
+      </Col>
+      <Col span="9">
+        Direita
+      </Col>
+    </Row>
+  </div>
+  );
+}
+
+export default App;
